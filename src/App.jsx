c493df7a2cc6cbbc46947cd72e9dab6cc7225127
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2022-11-03 13:23:23 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-gray-700 font-bold text-2xl">App</h1>
+    </div>
+  );
+};
+
+export default App;
