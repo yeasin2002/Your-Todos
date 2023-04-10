d6422891bce3b7cd65e0887f2c7c089cc85c2cbf
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-04-10 16:18:48 +0600

    resolving log in form

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 <h2 align="center" style="margin:10px 0px ; color: #132039 ">
  A task management application with the ability to collaborate with others
+ Todo-App is a kind of app that generally used to maintain our day-to-day tasks or list everything that we have to do, with the most important tasks at the top of the list, and the least important tasks at the bottom. It is helpful in planning our daily schedules 
 </h2>
 
 ## Figma UI
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -10,8 +14,11 @@ module.exports = {
         ashed: '#D9E1F2',
         'sky-250': '#C6D3EC',
         'Shades-100': '#D9E1F2',
+        'accent-10': '#d9e2f1',
+        'accent-20': '#c6d3eb',
+        'accent-50': '#436ebc',
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui'), require('flowbite/plugin')],
 };
